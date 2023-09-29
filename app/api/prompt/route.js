ff06279195869276cf634ff31ceb2086b7f4534b
dokesz <lacivagyok3@gmail.com>
2023-09-29 19:41:08 +0200

    dont show email from creator

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -3,19 +3,24 @@ import Prompt from "@models/prompt";
 
 //this is a get request that fetches all prompts from the database
 //it is an async function that takes in a request object
-//it connects to the database and then fetches all prompts
-
+//it connects to the database and then fetches all prompts 
 
 export const GET = async () => {
     try {
-        await connectToDatabase();
-        const prompts = await Prompt.find({}).populate('creator');
-        return new Response(JSON.stringify(prompts), { status: 200 });
+      await connectToDatabase();
+      const prompts = await Prompt.find({})
+        .populate("creator", "-email")
+        .select("-email")
+        .lean();
+      return new Response(JSON.stringify(prompts), { status: 200 });
     } catch (error) {
-        console.error("Failed to fetch all prompts:", error);
-        return new Response(JSON.stringify({ error: "Failed to fetch all prompts" }), { status: 500 });
+      console.error("Failed to fetch all prompts:", error);
+      return new Response(
+        JSON.stringify({ error: "Failed to fetch all prompts" }),
+        { status: 500 }
+      );
     }
-}
+};
 
 //how would this code would look like in express?
 // app.get('/api/prompt', async (req, res) => {
