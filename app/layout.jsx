bf751476f0e2796beb7474a4ae30b3ed41486695
dokesz <lacivagyok3@gmail.com>
2023-10-14 17:44:53 +0200

    warning message

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,27 @@
+"use client";
 import "../styles/globals.css";
 import Provider from "@components/Provider";
 import Nav from "@components/Nav";
+import { useSession } from "next-auth/react";
 
 export const metadata = {
   title: "Prompts",
   description: "Discover & Shae AI Prompts",
+};
+
+const SessionCheck = () => {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <p className="text-red-800 italic">
+        Note: If you sign-in, the app will store your email, name and profile
+        picture
+      </p>
+    );
+  }
+
+  return null;
 };
 
 const RootLayout = ({ children }) => {
@@ -18,6 +35,7 @@ const RootLayout = ({ children }) => {
 
           <main className="app">
             <Nav />
+            <SessionCheck />
             {children}
           </main>
         </Provider>
