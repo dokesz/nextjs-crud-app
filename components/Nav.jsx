737d33b8c1c17d4ee8f7081f731c60f697e468fb
dokesz <lacivagyok3@gmail.com>
2023-10-29 20:12:02 +0100

    forgot to add the buttons and image

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -40,50 +40,93 @@ const Nav = () => {
         <p className="logo_text">Prompt</p>
       </Link>
 
-      {(status === "loading" || loading) ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <div className="md:flex hidden">
-            {status === "authenticated" && (
-              <div className="flex gap-3 md:gap-5">
-                {/* Authenticated user options here... */}
-              </div>
-            )}
-            {status === "unauthenticated" && providers && (
-              <button
-                type="button"
-                onClick={() => {
-                  signIn(providers?.google.id);
-                }}
-                className="black_btn"
-              >
-                Sign in with Google
-              </button>
-            )}
+      <div className="md:flex hidden">
+        {status === "authenticated" && (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src={session?.user.image}
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
           </div>
+        )}
+        {status === "unauthenticated" && !loading && providers && (
+          <button
+            type="button"
+            onClick={() => {
+              signIn(providers?.google.id);
+            }}
+            className="black_btn"
+          >
+            Sign in with Google
+          </button>
+        )}
+      </div>
 
-          {/* Mobile Navigation */}
-          <div className="md:hidden flex relative">
-            {status === "authenticated" && (
-              <div className="flex">
-                {/* Mobile authenticated user options here... */}
+      {/* Mobile Navigation */}
+      <div className="md:hidden flex relative">
+        {status === "authenticated" && (
+          <div className="flex">
+            <Image
+              src={session?.user.image}
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => setToggleDropdown(!toggleDropdown)}
+            />
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
               </div>
             )}
-            {status === "unauthenticated" && providers && (
-              <button
-                type="button"
-                onClick={() => {
-                  signIn(providers?.google.id);
-                }}
-                className="black_btn"
-              >
-                Sign in with Google
-              </button>
-            )}
           </div>
-        </>
-      )}
+        )}
+        {status === "unauthenticated" && !loading && providers && (
+          <button
+            type="button"
+            onClick={() => {
+              signIn(providers?.google.id);
+            }}
+            className="black_btn"
+          >
+            Sign in with Google
+          </button>
+        )}
+      </div>
     </nav>
   );
 };
