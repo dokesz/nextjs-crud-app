81485e4409a31be1b36bb620b43ae67209194a92
dokesz <lacivagyok3@gmail.com>
2023-10-29 20:04:51 +0100

    possible fix for sing-in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const [toggleDropdown, setToggleDropdown] = useState(false);
   const [providers, setProviders] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -17,19 +17,15 @@ const Nav = () => {
       if (cachedProviders) {
         setProviders(cachedProviders);
         setLoading(false);
+      } else {
+        getProviders().then((res) => {
+          localStorage.setItem("provider", JSON.stringify(res));
+          setProviders(res);
+          setLoading(false);
+        });
       }
     }
   }, []);
-
-  useEffect(() => {
-    if (loading) {
-      getProviders().then((res) => {
-        localStorage.setItem("provider", JSON.stringify(res));
-        setProviders(res);
-        setLoading(false);
-      });
-    }
-  }, [loading]);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -44,90 +40,17 @@ const Nav = () => {
         <p className="logo_text">Prompt</p>
       </Link>
 
-      {/* Desktop Navigation */}
-      <div className="md:flex hidden">
-        {session?.user ? (
-          <div className="flex gap-3 md:gap-5">
-            <Link href="/create-prompt" className="black_btn">
-              Create Post
-            </Link>
-
-            <button type="button" onClick={signOut} className="outline_btn">
-              Sign Out
-            </button>
-
-            <Link href="/profile">
-              <Image
-                src={session?.user.image}
-                width={37}
-                height={37}
-                className="rounded-full"
-                alt="profile"
-              />
-            </Link>
-          </div>
-        ) : (
-          <>
-            {providers && !loading && (
-              <button
-                type="button"
-                onClick={() => {
-                  signIn(providers?.google.id);
-                }}
-                className="black_btn"
-              >
-                Sign in with Google
-              </button>
-            )}
-          </>
-        )}
-      </div>
-
-      {/* Mobile Navigation */}
-      <div className="md:hidden flex relative">
-        {session?.user ? (
-          <div className="flex">
-            <Image
-              src={session?.user.image}
-              width={37}
-              height={37}
-              className="rounded-full"
-              alt="profile"
-              onClick={() => setToggleDropdown(!toggleDropdown)}
-            />
-
-            {toggleDropdown && (
-              <div className="dropdown">
-                <Link
-                  href="/profile"
-                  className="dropdown_link"
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  My Profile
-                </Link>
-                <Link
-                  href="/create-prompt"
-                  className="dropdown_link"
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  Create Prompt
-                </Link>
-                <button
-                  type="button"
-                  onClick={() => {
-                    setToggleDropdown(false);
-                    signOut();
-                  }}
-                  className="mt-5 w-full black_btn"
-                >
-                  Sign Out
-                </button>
+      {(status === "loading" || loading) ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <div className="md:flex hidden">
+            {status === "authenticated" && (
+              <div className="flex gap-3 md:gap-5">
+                {/* Authenticated user options here... */}
               </div>
             )}
-          </div>
-        ) : (
-          <>
-            {providers && !loading && (
+            {status === "unauthenticated" && providers && (
               <button
                 type="button"
                 onClick={() => {
@@ -138,9 +61,29 @@ const Nav = () => {
                 Sign in with Google
               </button>
             )}
-          </>
-        )}
-      </div>
+          </div>
+
+          {/* Mobile Navigation */}
+          <div className="md:hidden flex relative">
+            {status === "authenticated" && (
+              <div className="flex">
+                {/* Mobile authenticated user options here... */}
+              </div>
+            )}
+            {status === "unauthenticated" && providers && (
+              <button
+                type="button"
+                onClick={() => {
+                  signIn(providers?.google.id);
+                }}
+                className="black_btn"
+              >
+                Sign in with Google
+              </button>
+            )}
+          </div>
+        </>
+      )}
     </nav>
   );
 };
